5ffe6e174c1ebe901898b35773a63f721a3f190c
Crowned08 <87036156+Crowned08@users.noreply.github.com>
2024-05-22 07:57:44 -0600

    Primer commit del proyecto

---
+++ Downloads/Tienda_k/src/test/java/com/Tienda_k/demo/TiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tienda_k.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
